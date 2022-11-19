e3a34289074f283e08755eb98b1c54923d0dd36c
Matteo Lorenzoni <mtt.lorenzoni@gmail.com>
2022-11-19 13:22:08 +0100

    Add material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './page/login/login.component';
@@ -8,7 +9,7 @@ import { PageNotFoundComponent } from './page/page-not-found/page-not-found.comp
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, PageNotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
