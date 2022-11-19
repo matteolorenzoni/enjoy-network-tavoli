5f0aba570bdaee79c7509700739d8d5b5b17901f
Matteo Lorenzoni <mtt.lorenzoni@gmail.com>
2022-11-19 13:36:11 +0100

    create folder: router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from '../router/app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './page/login/login.component';
 import { PageNotFoundComponent } from './page/page-not-found/page-not-found.component';
--- src/router/app-routing.module.ts
+++ src/router/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './page/login/login.component';
-import { PageNotFoundComponent } from './page/page-not-found/page-not-found.component';
+import { LoginComponent } from '../app/page/login/login.component';
+import { PageNotFoundComponent } from '../app/page/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
