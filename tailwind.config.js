dcf4bdcc0359b15d933fe9a6af9ec319bc831c3a
Matteo Lorenzoni <mtt.lorenzoni@gmail.com>
2022-11-19 19:04:24 +0100

    Refactor scss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      flexGrow: {
+        1: 1,
+        2: 2,
+        3: 3,
+        4: 4,
+        5: 5,
+        6: 6,
+        7: 7,
+        8: 8,
+        9: 9,
+        10: 10
+      }
+    },
+    fontFamily: {
+      RobotoMono: ['Roboto Mono']
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
+
+// Breakpoint
+// - sm: '640px',
+// - md: '768px',
+// - lg: '1024px',
+// - xl: '1280px',
+// - 2xl: '1536px',
