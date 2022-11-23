c1353139db1d19c13f6dab72df15e8c5f3cbb3cb
Matteo Lorenzoni <mtt.lorenzoni@gmail.com>
2022-11-23 19:22:56 +0100

    Update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./src/**/*.{html,js}', './src/app/components/en-button/*.ts'],
   theme: {
     extend: {
+      colors: {
+        blackPalette: {
+          100: '#ffffff',
+          200: '#dfdfdf',
+          300: '#bfbfbf',
+          400: '#9f9f9f',
+          500: '#808080',
+          600: '#606060',
+          700: '#404040',
+          800: '#202020',
+          900: '#000000',
+          DEFAULT: '#000000'
+        },
+        orangePalette: {
+          100: '#ffe0b2',
+          200: '#ffcc80',
+          300: '#ffb74d',
+          400: '#ffa726',
+          500: '#ff9800',
+          600: '#fb8c00',
+          700: '#f57c00',
+          800: '#ef6c00',
+          900: '#e65100',
+          DEFAULT: '#ff8a08'
+        }
+      },
       flexGrow: {
         1: 1,
         2: 2,
