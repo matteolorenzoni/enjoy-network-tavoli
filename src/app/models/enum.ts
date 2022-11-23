b10d55b8251bd0dfba3bbc743505fb2e65c8a42e
Matteo Lorenzoni <mtt.lorenzoni@gmail.com>
2022-11-23 23:27:44 +0100

    Models | Enum | Create file

---
+++ src/app/models/enum.ts
@@ -0,0 +1,30 @@
+export enum InputType {
+  BUTTON = 'button',
+  CHECKBOX = 'checkbox',
+  COLOR = 'color',
+  DATE = 'date',
+  DATETIME = 'datetime-local',
+  EMAIL = 'email',
+  FILE = 'file',
+  HIDDEN = 'hidden',
+  IMAGE = 'image',
+  MONTH = 'month',
+  NUMBER = 'number',
+  PASSWORD = 'password',
+  RADIO = 'radio',
+  RANGE = 'range',
+  RESET = 'reset',
+  SEARCH = 'search',
+  SUBMIT = 'submit',
+  TEL = 'tel',
+  TEXT = 'text',
+  TIME = 'time',
+  URL = 'url',
+  WEEK = 'week'
+}
+
+export enum PaletteType {
+  PRIMARY = 'primary',
+  SECONDARY = 'secondary',
+  OUTLINE = 'outline'
+}
