ed941b2edaa54bf5e99a94e696dd6f38f8942b51
Matteo Lorenzoni <mtt.lorenzoni@gmail.com>
2022-11-28 00:25:02 +0100

    Component | En-Field | Update component structure

--- src/app/components/en-field.component.ts
+++ src/app/components/en-field.component.ts
@@ -1,29 +1,41 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, FormGroup } from '@angular/forms';
 import { IconDefinition } from '@fortawesome/fontawesome-common-types/index';
 import { InputType, PaletteType } from '../models/enum';
 
 @Component({
-  selector: 'en-field',
+  selector: 'en-field[fieldName]',
   template: `
-    <div class="relative">
-      <div *ngIf="icon" class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3 ">
-        <fa-icon [icon]="icon"></fa-icon>
+    <div [ngClass]="['container', 'mx-auto', 'theme-' + palette]">
+      <div class="relative">
+        <div *ngIf="filedIcon" class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3 ">
+          <fa-icon [icon]="filedIcon" class="text-palette"></fa-icon>
+        </div>
+        <input
+          [type]="fieldType"
+          [name]="fieldName"
+          [disabled]="isDisabled"
+          [value]="value"
+          (input)="onChanged($event)"
+          (blur)="onBlur()"
+          [placeholder]="fieldPlaceholder"
+          class="
+            block
+            w-full
+            rounded-lg
+            bg-palette
+            p-4
+            pl-10
+            text-palette
+            placeholder-gray-200
+            focus:outline-none" />
       </div>
-      <input
-        [type]="fieldType"
-        [ngClass]="palette === 'primary' ? 'text-white' : 'text-blackPalette'"
-        class="
-          bg-black-900
-          block
-          w-full
-          rounded-lg
-          p-4
-          pl-10
-          text-white
-          placeholder-orange-500
-          focus:outline-none"
-        [placeholder]="placeholder"
-        required />
+      <!-- Da aggiungere al componente input => [ngClass]="{ 'has-error': formField?.invalid && formField?.dirty }"
+        <label [for]="fieldName">
+          <span class="mt-1 ml-1 flex items-center text-xs font-medium tracking-wide text-red-500">
+            Questo campo è obbligatorio
+        </span>
+      </label> -->
     </div>
   `,
   styles: [
@@ -33,11 +45,62 @@ import { InputType, PaletteType } from '../models/enum';
         width: 100%;
       }
     `
+  ],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => EnFieldComponent),
+      multi: true
+    }
   ]
 })
-export class EnFieldComponent {
-  @Input() fieldType: `${InputType}` = InputType.TEXT;
-  @Input() icon?: IconDefinition;
-  @Input() placeholder?: string;
+export class EnFieldComponent implements OnInit, ControlValueAccessor {
+  @Input() fieldName!: string;
+  @Input() fieldType?: `${InputType}` = InputType.TEXT;
+  @Input() filedIcon?: IconDefinition;
+  @Input() fieldPlaceholder?: string;
   @Input() palette?: `${PaletteType}` = PaletteType.PRIMARY;
+  @Input() parentForm?: FormGroup;
+
+  /** For form control */
+  public value: any = null;
+  public changed!: (value: string | number) => void;
+  public touched!: () => void;
+  public isDisabled = false;
+
+  constructor() {
+    // do nothing
+  }
+
+  ngOnInit(): void {}
+
+  // get formField(): FormControl {
+  //   return this.parentForm?.get(this.fieldName) as FormControl;
+  // }
+
+  public onChanged(event: Event): void {
+    const { value } = <HTMLInputElement>event.target;
+    this.changed(value);
+  }
+
+  public onBlur(): void {
+    console.log('sium');
+    this.touched();
+  }
+
+  public writeValue(value: any): void {
+    this.value = value;
+  }
+
+  public registerOnChange(fn: any): void {
+    this.changed = fn;
+  }
+
+  public registerOnTouched(fn: any): void {
+    this.touched = fn;
+  }
+
+  public setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
 }
--- src/app/page/login/login-form/login-form.component.ts
+++ src/app/page/login/login-form/login-form.component.ts
@@ -1,19 +1,45 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { faKey, faUser } from '@fortawesome/free-solid-svg-icons';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { faAt, faKey } from '@fortawesome/free-solid-svg-icons';
+import { InputType } from '../../../models/enum';
 
 @Component({
   selector: 'app-login-form[setStepEvent]',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss']
 })
-export class LoginFormComponent {
+export class LoginFormComponent implements OnInit {
   @Output() setStepEvent = new EventEmitter<boolean>();
 
   /** Icons */
-  nameIcon = faUser;
+  emailIcon = faAt;
   passwordIcon = faKey;
 
-  hideForm() {
-    this.setStepEvent.emit(true);
+  /** Variables */
+  passwordFieldType: InputType = InputType.PASSWORD;
+
+  /** Form */
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
+  });
+
+  constructor() {
+    // do nothing
+  }
+
+  ngOnInit(): void {}
+
+  // TODO: testare con poco tempo a disposizione
+  public onSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+      this.setStepEvent.emit(true);
+    }
+  }
+
+  // TODO: da aggiungere al custom field, ma opzionale
+  public togglePasswordVisibility(): void {
+    this.passwordFieldType = this.passwordFieldType === InputType.PASSWORD ? InputType.TEXT : InputType.PASSWORD;
   }
 }
