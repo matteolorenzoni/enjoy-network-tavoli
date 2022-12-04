dc97150667ffbbf0526e7ca5d6c6eb6a18a20432
Matteo Lorenzoni <mtt.lorenzoni@gmail.com>
2022-12-04 14:23:21 +0100

    Page | Event | Add floating button

--- src/app/page/administrator/event/event.component.ts
+++ src/app/page/administrator/event/event.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
   styleUrls: ['./event.component.scss']
 })
-export class EventComponent {}
+export class EventComponent implements OnInit {
+  /* Icons */
+  plusIcon = faPlus;
+
+  constructor() {
+    // do nothing
+  }
+
+  ngOnInit(): void {
+    console.log('qui scarico la lista di eventi gia creati');
+  }
+
+  goToCreateEvent(): void {
+    console.log('qui vado alla pagina di creazione evento');
+  }
+}
