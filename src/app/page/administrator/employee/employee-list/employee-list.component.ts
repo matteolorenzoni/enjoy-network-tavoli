727f945e7f5f8ed7f710d307f4ab3e2cc51aac61
Matteo Lorenzoni <mtt.lorenzoni@gmail.com>
2022-12-10 17:01:36 +0100

    Page | Employee-list | Add filter and floating buttons

--- src/app/page/administrator/employee/employee-list/employee-list.component.ts
+++ src/app/page/administrator/employee/employee-list/employee-list.component.ts
@@ -1,8 +1,34 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { faFilter, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { fadeInAnimation } from 'src/app/animations/animations';
+import { Employee } from 'src/app/models/type';
+import { ToastService } from 'src/app/services/toast.service';
+import { EmployeeService } from '../../../../services/employee.service';
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
-  styleUrls: ['./employee-list.component.scss']
+  styleUrls: ['./employee-list.component.scss'],
+  animations: [fadeInAnimation]
 })
-export class EmployeeListComponent {}
+export class EmployeeListComponent {
+  /* Icons */
+  plusIcon = faPlus;
+  filterIcon = faFilter;
+
+  /* Employees */
+  employees: Employee[] = [];
+
+  constructor(private router: Router, private employeeService: EmployeeService, private toastService: ToastService) {}
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  goToCreateEmployee(): void {
+    this.router.navigate(['create-item/employee/null']);
+  }
+
+  getEmployees(): void {}
+}
