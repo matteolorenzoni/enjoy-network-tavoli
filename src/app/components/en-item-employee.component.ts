919a9cdb324b416bfd70c4f73107532b6a608904
Matteo Lorenzoni <mtt.lorenzoni@gmail.com>
2022-12-24 14:36:42 +0100

    Component | En-item-employee | Fix style

--- src/app/components/en-item-employee.component.ts
+++ src/app/components/en-item-employee.component.ts
@@ -8,7 +8,7 @@ import { ToastService } from '../services/toast.service';
 @Component({
   selector: 'app-en-item-employee[employee]',
   template: `
-    <li class="flex h-14 items-center">
+    <li class="flex h-14 items-center gap-2">
       <ng-container *ngIf="employeeDTO.active; else elseTemplate">
         <fa-icon [icon]="activeIcon" class="text-emerald-600"></fa-icon>
       </ng-container>
@@ -22,11 +22,11 @@ import { ToastService } from '../services/toast.service';
       <div class="ml-auto shrink-0 px-1">
         <fa-icon
           [icon]="modifyIcon"
-          class="mr-1 text-gray-500  hover:cursor-pointer hover:text-gray-600 active:text-gray-800"
+          class="mr-2 text-gray-500  hover:cursor-pointer hover:text-gray-600 active:text-gray-800"
           [routerLink]="['/create-item/employee', uid]"></fa-icon>
         <fa-icon
           [icon]="deleteIcon"
-          class="ml-1 text-gray-500 hover:cursor-pointer hover:text-gray-600 active:text-gray-800"
+          class="ml-2 text-gray-500 hover:cursor-pointer hover:text-gray-600 active:text-gray-800"
           (click)="deleteEmployee()"></fa-icon>
       </div>
     </li>
