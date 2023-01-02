0865bc19042182c9398d961598ebdb26abfdbe33
Matteo Lorenzoni <mtt.lorenzoni@gmail.com>
2023-01-02 15:24:33 +0100

    Component | En-countdown | Use real date

--- src/app/components/en-countdown.component.ts
+++ src/app/components/en-countdown.component.ts
@@ -1,24 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'en-countdown',
+  selector: 'en-countdown[date]',
   template: `
-    <div class="center grid auto-cols-max grid-flow-col gap-5 text-center">
+    <div *ngIf="date" class="center grid auto-cols-max grid-flow-col gap-5 text-center">
       <div class="rounded-box flex flex-col bg-neutral p-2 text-neutral-content">
         <span class="font-mono countdown text-5xl">
-          <span [style]="'--value: ' + 15"></span>
+          <span [style]="'--value: ' + days"></span>
         </span>
         GIORNI
       </div>
       <div class="rounded-box flex flex-col bg-neutral p-2 text-neutral-content">
         <span class="font-mono countdown text-5xl">
-          <span [style]="'--value: ' + 10"></span>
+          <span [style]="'--value: ' + hours"></span>
         </span>
         ORE
       </div>
       <div class="rounded-box flex flex-col bg-neutral p-2 text-neutral-content">
         <span class="font-mono countdown text-5xl">
-          <span [style]="'--value: ' + 24"></span>
+          <span [style]="'--value: ' + minutes"></span>
         </span>
         MINUTI
       </div>
@@ -32,4 +33,28 @@ import { Component } from '@angular/core';
     `
   ]
 })
-export class EnCountdownComponent {}
+export class EnCountdownComponent implements OnChanges {
+  @Input() date!: Date | null;
+
+  /* Values */
+  currentDay = new Date();
+  days = 0;
+  hours = 0;
+  minutes = 0;
+
+  /* Subscriptions */
+  countdownSubscription: Subscription | null = null;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['date']) {
+      const currentValue = changes['date'].currentValue as Date;
+      const diff = currentValue.getTime() - this.currentDay.getTime();
+      const days = diff / (1000 * 60 * 60 * 24);
+      const hours = diff / (1000 * 60 * 60);
+      const minutes = diff / (1000 * 60);
+      this.days = Math.floor(days);
+      this.hours = Math.floor(hours - this.days * 24);
+      this.minutes = Math.floor(minutes - this.hours * 60 - this.days * 24 * 60);
+    }
+  }
+}
