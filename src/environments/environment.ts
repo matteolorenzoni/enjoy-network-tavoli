31304b4f14c1bad83c0a08a739e0f56d39e584d1
Matteo Lorenzoni <mtt.lorenzoni@gmail.com>
2023-02-27 19:21:38 +0100

    Environment | Update production value

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: !false,
+  production: false,
   firebase: {
     projectId: 'enjoy-network-tavoli',
     appId: '1:682704062987:web:bfebc270cfdb2fd2e3550e',
