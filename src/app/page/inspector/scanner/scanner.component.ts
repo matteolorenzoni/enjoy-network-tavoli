2b5328ea3c86cdbd3a1201d86a6422c4b294ff51
Matteo Lorenzoni <mtt.lorenzoni@gmail.com>
2023-04-01 19:46:08 +0200

    Page | Scanner | Disabled camera when checking a participation

--- src/app/page/inspector/scanner/scanner.component.ts
+++ src/app/page/inspector/scanner/scanner.component.ts
@@ -197,6 +197,8 @@ export class ScannerComponent {
 
   /* ------------------------------------ Methods ------------------------------------ */
   getParticipation(participationUid: string) {
+    this.enabled = false;
+
     const participationNotScannedYet = this.participationsActive.find(
       (participation) => participation.uid === participationUid
     );
@@ -239,6 +241,8 @@ export class ScannerComponent {
   }
 
   onResetParticipation() {
+    this.enabled = Boolean(this.currentDevice);
+
     if (this.currentDevice) {
       this.cameraContainer.nativeElement.style.display = 'flex';
       this.infoContainer.nativeElement.style.display = 'none';
