892b9e7aaf05e97fc46652829e8d11fbf09672a4
Matteo Lorenzoni <mtt.lorenzoni@gmail.com>
2023-04-07 22:33:30 +0200

    Component | En-item-available | Graphic fix

--- src/app/components/en-item-event-available.component.ts
+++ src/app/components/en-item-event-available.component.ts
@@ -10,7 +10,7 @@ import { Event } from 'src/app/models/type';
       class="group relative my-4 mx-auto flex w-full flex-col rounded-xl bg-gray-900 p-2 shadow shadow-gray-800 transition-all hover:cursor-pointer sm:w-3/5"
       (click)="goToSelector()">
       <img [src]="event.props.imageUrl" alt="foto-evento" class="mb-4 aspect-square rounded-xl object-contain " />
-      <div class="absolute left-0 bottom-0 flex p-2 backdrop-blur">
+      <div class="absolute left-0 bottom-0 flex w-full p-2 backdrop-blur">
         <div>
           <p class="pb-2 text-xl font-medium text-primary-60">{{ event.props.name }}</p>
           <p class="text-xs font-light text-slate-300">{{ event.props.place }}</p>
