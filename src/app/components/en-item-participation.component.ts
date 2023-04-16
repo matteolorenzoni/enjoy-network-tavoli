b24084b56dfbed365639229f1238115296388065
Matteo Lorenzoni <mtt.lorenzoni@gmail.com>
2023-04-16 12:26:43 +0200

    Component | En-item-participation | Graphic fix

--- src/app/components/en-item-participation.component.ts
+++ src/app/components/en-item-participation.component.ts
@@ -1,4 +1,11 @@
-import { faArrowUpFromBracket, faCopy, faTrash } from '@fortawesome/free-solid-svg-icons';
+import {
+  faArrowUpFromBracket,
+  faCalendar,
+  faCopy,
+  faPhone,
+  faQrcode,
+  faTrash
+} from '@fortawesome/free-solid-svg-icons';
 import { Component, Input } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
@@ -11,34 +18,38 @@ import { Participation } from '../models/type';
   selector: 'en-item-participation[participation]',
   template: `
     <li
-      class="my-2 flex h-12 items-center rounded-lg px-2 text-slate-300"
+      class="flex items-center gap-4 rounded-md p-2 text-slate-300"
       [ngClass]="{
-        'bg-emerald-400/50': participation.props.scannedAt && participation.props.isActive,
+        'bg-slate-900': participation.props.messageIsReceived && participation.props.isActive,
         'bg-red-700': !participation.props.messageIsReceived && participation.props.isActive,
+        'bg-emerald-400/50': participation.props.scannedAt && participation.props.isActive,
         'bg-slate-800 opacity-20': !participation.props.isActive
       }">
-      <div class="grow truncate">
-        <p>
-          {{ participation.props.name }} {{ participation.props.lastName }}
-          <span class="ml-4 truncate text-xs">({{ participation.props.phone }})</span>
+      <div class="flex grow flex-col gap-1 truncate">
+        <p class="truncate">{{ participation.props.name }} {{ participation.props.lastName }}</p>
+
+        <p class="truncate text-xs text-slate-400">
+          <fa-icon class="pr-1" [icon]="phoneIcon"></fa-icon>
+          {{ participation.props.phone }}
         </p>
 
         <ng-container *ngIf="participation.props.scannedAt; else elseTemplate">
-          <p class="text-xs text-slate-400">
-            ENTRATO IL:
+          <p class="truncate text-xs text-slate-400">
+            <fa-icon class="pr-1" [icon]="qrCodeIcon"></fa-icon>
             {{ participation.props.scannedAt | date: 'dd/MM/YYYY' }} -
             {{ participation.props.scannedAt | date: 'HH:mm' }}
           </p>
         </ng-container>
         <ng-template #elseTemplate>
-          <p class="text-xs text-slate-400">
+          <p class="truncate text-xs text-slate-400">
+            <fa-icon class="pr-1" [icon]="calendarIcon"></fa-icon>
             {{ participation.props.modifiedAt | date: 'dd/MM/YYYY' }} -
             {{ participation.props.modifiedAt | date: 'HH:mm' }}
           </p>
         </ng-template>
       </div>
 
-      <div class="flex shrink-0 gap-4 pl-4">
+      <div class="flex shrink-0 gap-4">
         <fa-icon
           *ngIf="canShare"
           [icon]="shareIcon"
@@ -80,6 +91,9 @@ export class EnItemParticipationComponent {
   shareIcon = faArrowUpFromBracket;
   copyIcon = faCopy;
   deleteIcon = faTrash;
+  phoneIcon = faPhone;
+  calendarIcon = faCalendar;
+  qrCodeIcon = faQrcode;
 
   /* Subscriptions */
   subIsActive!: Subscription;
