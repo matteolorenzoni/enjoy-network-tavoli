f7048cc94ff0d65a3b672b269abd804f4e45cb70
Matteo Lorenzoni <mtt.lorenzoni@gmail.com>
2023-04-23 14:17:08 +0200

    Service | Firebase-delete | Add loader

--- src/app/services/firebase/firebase-crud/firebase-delete.service.ts
+++ src/app/services/firebase/firebase-crud/firebase-delete.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Firestore, doc, deleteDoc, getFirestore, collection, writeBatch } from '@angular/fire/firestore';
+import { LoaderService } from '../../loader.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,17 +9,23 @@ export class FirebaseDeleteService {
   /* Firebase */
   private db!: Firestore;
 
-  constructor() {
+  constructor(private loaderService: LoaderService) {
     this.db = getFirestore();
   }
 
   public async deleteDocumentByUid(collectionName: string, documentUid: string): Promise<void> {
+    this.loaderService.show();
+
     const collectionRef = collection(this.db, collectionName);
     const docRef = doc(collectionRef, documentUid);
     await deleteDoc(docRef);
+
+    this.loaderService.hide();
   }
 
   public async deleteDocumentsByUids(collectionName: string, documentUids: string[]): Promise<void> {
+    this.loaderService.show();
+
     const batch = writeBatch(this.db);
     const collectionRef = collection(this.db, collectionName);
     documentUids.forEach((documentUid: string) => {
@@ -26,5 +33,7 @@ export class FirebaseDeleteService {
       batch.delete(docRef);
     });
     await batch.commit();
+
+    this.loaderService.hide();
   }
 }
