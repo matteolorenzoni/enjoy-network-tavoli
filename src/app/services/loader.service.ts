cc93f8828b7383a5e7ecc7f40ef658c4c17ebc4b
Matteo Lorenzoni <mtt.lorenzoni@gmail.com>
2023-04-23 14:57:35 +0200

    Service | Loader | DIsable user interaction when loader is active

--- src/app/services/loader.service.ts
+++ src/app/services/loader.service.ts
@@ -9,9 +9,17 @@ export class LoaderService {
 
   show(): void {
     this.loader$.next(true);
+
+    document.body.style.overflow = 'hidden';
+    document.body.style.height = '100vh';
+    document.body.style.touchAction = 'none';
   }
 
   hide(): void {
     this.loader$.next(false);
+
+    document.body.style.overflow = 'auto';
+    document.body.style.height = 'auto';
+    document.body.style.touchAction = 'auto';
   }
 }
