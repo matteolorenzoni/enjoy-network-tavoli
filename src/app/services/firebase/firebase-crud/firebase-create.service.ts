b3dcbf30bef237dc101d7f861797c6d4cbb926e1
Matteo Lorenzoni <mtt.lorenzoni@gmail.com>
2023-04-23 14:19:41 +0200

    Service | Firebase-create | Add loader

--- src/app/services/firebase/firebase-crud/firebase-create.service.ts
+++ src/app/services/firebase/firebase-crud/firebase-create.service.ts
@@ -6,12 +6,12 @@ import {
   addDoc,
   getFirestore,
   collection,
-  writeBatch,
   doc,
   setDoc
 } from '@angular/fire/firestore';
 import { Assignment, Client, CustomError, Employee, Event, Table } from 'src/app/models/type';
 import { Participation } from '../../../models/type';
+import { LoaderService } from '../../loader.service';
 
 @Injectable({
   providedIn: 'root'
@@ -20,7 +20,7 @@ export class FirebaseCreateService {
   /* Firebase */
   private db!: Firestore;
 
-  constructor() {
+  constructor(private loaderService: LoaderService) {
     this.db = getFirestore();
   }
 
@@ -28,6 +28,8 @@ export class FirebaseCreateService {
     collectionName: string,
     data: Event | Assignment | Employee | Table | Participation | Client | CustomError
   ): Promise<DocumentReference<DocumentData>> {
+    this.loaderService.show();
+
     const { props } = data;
 
     /* Sanitize props */
@@ -39,6 +41,8 @@ export class FirebaseCreateService {
 
     const collectionRef = collection(this.db, collectionName);
     const docRef = await addDoc(collectionRef, props);
+
+    this.loaderService.hide();
     return docRef;
   }
 
@@ -46,6 +50,8 @@ export class FirebaseCreateService {
     collectionName: string,
     data: Event | Assignment | Employee | Table | Participation | Client | CustomError
   ): Promise<DocumentReference<DocumentData>> {
+    this.loaderService.show();
+
     const { uid, props } = data;
 
     /* Sanitize props */
@@ -58,28 +64,9 @@ export class FirebaseCreateService {
     const collectionRef = collection(this.db, collectionName);
     const docRef = doc(collectionRef, uid);
     await setDoc(docRef, props);
+
+    this.loaderService.hide();
+
     return docRef;
-  }
-
-  public async addDocuments(
-    collectionName: string,
-    data: (Event | Assignment | Employee | Table | Participation | Client | CustomError)[]
-  ): Promise<void> {
-    const batch = writeBatch(this.db);
-    const collectionRef = collection(this.db, collectionName);
-    data.forEach((document: Event | Assignment | Employee | Table | Participation | Client | CustomError) => {
-      const { props } = document;
-
-      /* Sanitize props */
-      Object.entries(props).forEach(([key, value]) => {
-        if (value === null || value === undefined) delete (props as any)[key];
-      });
-      props.createdAt = new Date();
-      props.modifiedAt = new Date();
-
-      const docRef = doc(collectionRef);
-      batch.set(docRef, props);
-    });
-    await batch.commit();
   }
 }
