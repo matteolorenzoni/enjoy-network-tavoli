80a49db52f123694bdc4d88607c721038415b7f3
Matteo Lorenzoni <mtt.lorenzoni@gmail.com>
2023-05-02 23:53:37 +0200

    Page | Setting | Remove animation

--- src/app/page/setting/setting.component.ts
+++ src/app/page/setting/setting.component.ts
@@ -2,13 +2,11 @@ import { UserService } from 'src/app/services/user.service';
 import { Component } from '@angular/core';
 import { faKey, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 import { Router, ActivatedRoute } from '@angular/router';
-import { fadeInCreateItemAnimation } from 'src/app/animations/animations';
 
 @Component({
   selector: 'app-setting',
   templateUrl: './setting.component.html',
-  styleUrls: ['./setting.component.scss'],
-  animations: [fadeInCreateItemAnimation]
+  styleUrls: ['./setting.component.scss']
 })
 export class SettingComponent {
   /* Icons */
