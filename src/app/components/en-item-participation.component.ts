82f4059d97de4b17c051e284e7f0d09ed2bdacef
Matteo Lorenzoni <mtt.lorenzoni@gmail.com>
2023-05-07 14:45:06 +0200

    Component | En-item-participation | Swipe icon buttons

--- src/app/components/en-item-participation.component.ts
+++ src/app/components/en-item-participation.component.ts
@@ -1,4 +1,3 @@
-/* eslint-disable operator-linebreak */
 import {
   faArrowUpFromBracket,
   faCalendar,
@@ -8,7 +7,7 @@ import {
   faRotateRight,
   faTrash
 } from '@fortawesome/free-solid-svg-icons';
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { Component, ElementRef, Input, SimpleChanges, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { SessionStorageService } from '../services/sessionstorage.service';
@@ -20,73 +19,77 @@ import { Participation } from '../models/type';
   selector: 'en-item-participation[participation]',
   template: `
     <li
-      class="flex items-center gap-4 overflow-hidden rounded-md p-2 text-slate-300"
+      class="overflow-hidden rounded-md"
       [ngClass]="{
         'bg-slate-900': participation.props.messageIsReceived && participation.props.isActive,
         'bg-red-700': !participation.props.messageIsReceived && participation.props.isActive,
         'bg-emerald-400/50': participation.props.scannedAt && participation.props.isActive,
         'bg-slate-800 opacity-20': !participation.props.isActive
       }">
-      <div class="flex grow flex-col gap-1 truncate">
-        <p class="truncate">{{ participation.props.name }} {{ participation.props.lastName }}</p>
+      <div #itemRef class="relative flex cursor-pointer ">
+        <div class="flex w-full min-w-full items-center gap-4 p-2 text-slate-300" (click)="toggleIcons()">
+          <div class="flex grow flex-col gap-1 truncate">
+            <p class="truncate">{{ participation.props.name }} {{ participation.props.lastName }}</p>
 
-        <p class="truncate text-xs text-slate-400">
-          <fa-icon class="pr-1" [icon]="phoneIcon"></fa-icon>
-          {{ participation.props.phone }}
-        </p>
+            <p class="truncate text-xs text-slate-400">
+              <fa-icon class="pr-1" [icon]="phoneIcon"></fa-icon>
+              {{ participation.props.phone }}
+            </p>
 
-        <ng-container *ngIf="participation.props.scannedAt; else elseTemplate">
-          <p class="truncate text-xs text-slate-400">
-            <fa-icon class="pr-1" [icon]="qrCodeIcon"></fa-icon>
-            {{ participation.props.scannedAt | date: 'dd/MM/YYYY' }} -
-            {{ participation.props.scannedAt | date: 'HH:mm' }}
-          </p>
-        </ng-container>
-        <ng-template #elseTemplate>
-          <p class="truncate text-xs text-slate-400">
-            <fa-icon class="pr-1" [icon]="calendarIcon"></fa-icon>
-            {{ participation.props.modifiedAt | date: 'dd/MM/YYYY' }} -
-            {{ participation.props.modifiedAt | date: 'HH:mm' }}
-          </p>
-        </ng-template>
-      </div>
-
-      <div
-        *ngIf="!participation.props.messageIsReceived"
-        class="animate-pulse rounded bg-red-800 p-2 text-center text-xs">
-        <ng-container *ngIf="(participation.props.messageAttempt || 1) < 3; else elseTemplate">
-          <p>Tentativo n°{{ (participation.props.messageAttempt || 1) + 1 }} alle</p>
-          <p>
-            {{ newAttempt | date: 'dd/MM/YYYY' }} -
-            {{ newAttempt | date: 'HH:mm' }}
-          </p>
-        </ng-container>
-        <ng-template #elseTemplate>
-          <div role="button" (click)="maxAttemptInfo()">
-            <p>🚨Attenzione🚨</p>
-            <p>Clicca per info</p>
+            <ng-container *ngIf="participation.props.scannedAt; else elseTemplate">
+              <p class="truncate text-xs text-slate-400">
+                <fa-icon class="pr-1" [icon]="qrCodeIcon"></fa-icon>
+                {{ participation.props.scannedAt | date: 'dd/MM/YYYY' }} -
+                {{ participation.props.scannedAt | date: 'HH:mm' }}
+              </p>
+            </ng-container>
+            <ng-template #elseTemplate>
+              <p class="truncate text-xs text-slate-400">
+                <fa-icon class="pr-1" [icon]="calendarIcon"></fa-icon>
+                {{ participation.props.modifiedAt | date: 'dd/MM/YYYY' }} -
+                {{ participation.props.modifiedAt | date: 'HH:mm' }}
+              </p>
+            </ng-template>
           </div>
-        </ng-template>
-      </div>
 
-      <div class="flex shrink-0 gap-3 text-xl text-slate-300">
-        <fa-icon
-          *ngIf="canShare"
-          [icon]="shareIcon"
-          role="button"
-          class="p-1 transition duration-150 ease-in-out hover:cursor-pointer active:text-slate-500"
-          (click)="shareTicketLink(participation.uid)"></fa-icon>
-        <fa-icon
-          *ngIf="!canShare"
-          [icon]="copyIcon"
-          role="button"
-          class="p-1 transition duration-150 ease-in-out hover:cursor-pointer active:text-slate-500"
-          (click)="copyTicketLink(participation.uid)"></fa-icon>
-        <fa-icon
-          [icon]="deleteIcon"
-          role="button"
-          class="p-1 transition duration-150 ease-in-out hover:cursor-pointer active:text-slate-500"
-          (click)="updateParticipationNotActive()"></fa-icon>
+          <div
+            *ngIf="!participation.props.messageIsReceived"
+            class="animate-pulse rounded bg-red-800 p-2 text-center text-xs">
+            <ng-container *ngIf="(participation.props.messageAttempt || 1) < 3; else elseTemplate">
+              <p>Tentativo n°{{ (participation.props.messageAttempt || 1) + 1 }} alle</p>
+              <p>
+                {{ newAttempt | date: 'dd/MM/YYYY' }} -
+                {{ newAttempt | date: 'HH:mm' }}
+              </p>
+            </ng-container>
+            <ng-template #elseTemplate>
+              <div role="button" (click)="maxAttemptInfo()">
+                <p>🚨Attenzione🚨</p>
+                <p>Clicca per info</p>
+              </div>
+            </ng-template>
+          </div>
+        </div>
+
+        <div #iconDiv class="flex items-center gap-3 p-2 text-xl text-slate-300">
+          <fa-icon
+            *ngIf="canShare"
+            [icon]="shareIcon"
+            role="button"
+            class="p-1 transition duration-150 ease-in-out hover:cursor-pointer active:text-slate-500"
+            (click)="shareTicketLink(participation.uid)"></fa-icon>
+          <fa-icon
+            *ngIf="!canShare"
+            [icon]="copyIcon"
+            role="button"
+            class="p-1 transition duration-150 ease-in-out hover:cursor-pointer active:text-slate-500"
+            (click)="copyTicketLink(participation.uid)"></fa-icon>
+          <fa-icon
+            [icon]="deleteIcon"
+            role="button"
+            class="p-1 transition duration-150 ease-in-out hover:cursor-pointer active:text-slate-500"
+            (click)="updateParticipationNotActive()"></fa-icon>
+        </div>
       </div>
     </li>
   `,
@@ -100,6 +103,7 @@ import { Participation } from '../models/type';
 })
 export class EnItemParticipationComponent {
   @Input() participation!: Participation;
+  @ViewChild('itemRef') itemRef!: ElementRef<HTMLDivElement>;
 
   /* Event */
   eventUid: string | null = null;
@@ -107,6 +111,15 @@ export class EnItemParticipationComponent {
   /* Employee */
   employeeUid: string | null = null;
   newAttempt?: Date;
+
+  /* Subscriptions */
+  subIsActive!: Subscription;
+
+  /* Navigator */
+  canShare = false;
+
+  /* Item */
+  isOpen = false;
 
   /* Icons */
   rotateIcon = faRotateRight;
@@ -116,12 +129,6 @@ export class EnItemParticipationComponent {
   phoneIcon = faPhone;
   calendarIcon = faCalendar;
   qrCodeIcon = faQrcode;
-
-  /* Subscriptions */
-  subIsActive!: Subscription;
-
-  /* Navigator */
-  canShare = false;
 
   /* ------------------------------ Constructor ------------------------------ */
   constructor(
@@ -189,6 +196,12 @@ export class EnItemParticipationComponent {
           this.toastService.showError(error);
         });
     }
+  }
+
+  toggleIcons(): void {
+    this.itemRef.nativeElement.style.transform = `translateX(${this.isOpen ? 0 : -80}px)`;
+    this.itemRef.nativeElement.style.transition = 'transform 0.5s ease-in-out';
+    this.isOpen = !this.isOpen;
   }
 
   maxAttemptInfo(): void {
