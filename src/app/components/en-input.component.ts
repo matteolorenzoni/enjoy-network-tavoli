f960c713cad32b21e22834737020593e91b6cfd9
Matteo Lorenzoni <mtt.lorenzoni@gmail.com>
2023-05-20 22:39:49 +0200

    Component | En-input | Update style

--- src/app/components/en-input.component.ts
+++ src/app/components/en-input.component.ts
@@ -10,10 +10,12 @@ import { ControlContainer, FormGroupDirective } from '@angular/forms';
       </div>
       <input
         [type]="type"
+        [size]="size || 100"
+        [pattern]="pattern || '.*'"
         [name]="name + '_input'"
         [id]="name + '_input'"
-        class="block w-full rounded-lg border border-slate-700 bg-slate-800 p-4 pl-16 text-sm text-slate-200 placeholder-slate-500 focus:outline-none focus:ring-2"
-        [ngStyle]="{ 'padding-left': leftRemPadding > 0 ? leftRemPadding + 'rem' : '4rem' }"
+        class="block w-full rounded-lg border border-slate-700 bg-slate-800 p-4 pl-16 text-sm text-slate-200 placeholder-slate-500 focus:outline-none focus:ring-2 disabled:cursor-not-allowed disabled:border-gray-900 disabled:bg-gray-900 disabled:placeholder:text-gray-900"
+        [ngStyle]="{ 'padding-left': leftRemPadding ? leftRemPadding + 'rem' : '4rem' }"
         [formControlName]="fc"
         autocomplete="off"
         [placeholder]="placeholder || ''"
@@ -33,9 +35,11 @@ import { ControlContainer, FormGroupDirective } from '@angular/forms';
 export class EnInputComponent {
   @Input() fieldName!: string;
   @Input() type = 'text';
+  @Input() pattern?: string | RegExp;
+  @Input() size?: number;
   @Input() name!: string;
   @Input() placeholder?: string;
   @Input() fc!: string;
   @Input() required = false;
-  @Input() leftRemPadding = 0;
+  @Input() leftRemPadding?: number;
 }
